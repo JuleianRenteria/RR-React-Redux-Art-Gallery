d803165fd80d46e8788c8d0137c7485fcd8f6cc3
Juleian Renteria <jprenteria02@gmail.com>
2023-04-19 17:08:27 -0700

    finished

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit'
+import dataReducer from './src/dataSlice'
+
+export const store = configureStore({
+    reducer: {
+        data:dataReducer
+    }
+})
